4c238ad466acacb68ebab2e65a56534e3058dced
Abhishek <abhi736902@gmail.com>
2025-09-15 13:54:21 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -164,7 +164,7 @@ async def cmd(client, callback_query):
     ])
     await callback_query.message.edit_media(
     InputMediaPhoto(
-      media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+      media="https://i.postimg.cc/3w21B0XN/backiee-230452-landscape.jpg",
       caption=caption
     ),
     reply_markup=keyboard
@@ -203,7 +203,7 @@ async def help_button(client, callback_query):
     
   await callback_query.message.edit_media(
     InputMediaPhoto(
-      media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+      media="https://i.postimg.cc/3w21B0XN/backiee-230452-landscape.jpg",
       caption=caption
     ),
     reply_markup=keyboard
