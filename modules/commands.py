976d682232091acf781430347a6262cd5e8fd299
Abhishek <abhi736902@gmail.com>
2025-09-18 13:17:06 +0530

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -26,7 +26,7 @@ def register_commands_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://i.postimg.cc/pT8MFMF2/backiee-330776-landscape.jpg",
           caption=caption
         ),
         reply_markup=keyboard
@@ -65,7 +65,7 @@ def register_commands_handlers(bot):
     
       await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://i.postimg.cc/pT8MFMF2/backiee-330776-landscape.jpg",
           caption=caption
         ),
         reply_markup=keyboard
@@ -92,7 +92,7 @@ def register_commands_handlers(bot):
     
       await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://i.postimg.cc/pT8MFMF2/backiee-330776-landscape.jpg",
           caption=caption
         ),
         reply_markup=keyboard
